3acecfdc2c75a2f8230f228b2c3c329e912c404c
carbonleakage <carbonleakage@outlook.com>
2026-06-18 14:32:45 +0200

    added falling squares

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 use macroquad::prelude::*;
 
+struct Shape {
+    size: f32,
+    speed: f32,
+    x: f32,
+    y: f32,
+}
+
 #[macroquad::main("Sarav's Game")]
 async fn main() {
     const MOVEMENT_SPEED: f32 = 200.0;
+
+    rand::srand(miniquad::date::now() as u64);
+    let mut squares = vec![];
+    let mut circle = Shape {
+        size: 32.0,
+        speed: MOVEMENT_SPEED,
+        x: screen_width()/2.0,
+        y: screen_height()/2.0,
+    };
 
     let mut x = screen_width()/2.0;
     let mut y = screen_height()/2.0;
@@ -23,9 +39,28 @@ async fn main() {
             y += MOVEMENT_SPEED * delta_time;
         }
 
-        x = clamp(x,0.0, screen_width());
-        y = clamp(y,0.0, screen_height());
+        circle.x = clamp(circle.x,0.0, screen_width());
+        circle.y = clamp(circle.y,0.0, screen_height());
+
+        if rand::gen_range(0,99) >= 95 {
+            let size = rand::gen_range(16.0, 64.0);
+            squares.push(Shape {
+                size,
+                speed: rand::gen_range(50.0, 150.0),
+                x: rand::gen_range(size/2.0, screen_width() - size/2.0),
+                y: -size,
+            });
+        }
+
+        for square in &mut squares {
+            square.y += square.speed * delta_time;
+        }
+        squares.retain(|square| square.y < screen_height() + square.size);
+
         draw_circle(x,y,16.0, YELLOW);
+        for square in &squares {
+            draw_rectangle(square.x-square.size/2.0, square.y - square.size/2.0,square.size, square.size,GREEN);
+        }
         next_frame().await
     }
 }
